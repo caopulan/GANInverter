486302a3bb3ed42eb3afb8675509c4738a161ac6
sariel <2788787973@qq.com>
2023-04-24 10:48:07 +0800

    add id evaluation

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -1,6 +1,9 @@
 model_paths = {
 	'ir_se50': 'pretrained_models/model_ir_se50.pth',
 	'circular_face': 'pretrained_models/CurricularFace_Backbone.pth',
+	'mtcnn_pnet': 'pretrained_models/mtcnn/pnet.npy',
+	'mtcnn_rnet': 'pretrained_models/mtcnn/rnet.npy',
+	'mtcnn_onet': 'pretrained_models/mtcnn/onet.npy',
 	'shape_predictor': 'shape_predictor_68_face_landmarks.dat',
 	'moco': 'pretrained_models/moco_v2_800ep_pretrain.pth.tar',
 	'invert_predictor_faces': 'pretrained_models/invertibility_faces_sg2.pt',
